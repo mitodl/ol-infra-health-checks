9524c2f1ba776e05ac5fc8552894e35f21bc038f
Chris Patti <feoh@feoh.org>
2024-01-12 17:40:41 -0500

    Fix: use check_output rather than check_call
    Also strip newline from output of docker compose ps -q

--- tests/test_xpro_health_checks.py
+++ tests/test_xpro_health_checks.py
@@ -1,4 +1,23 @@
-def test_xpro_lms(host):
-    lms = host.docker("lms")
-    assert lms.is_running
-    assert lms.is_enabled
+import subprocess
+import pytest
+import testinfra
+
+
+@pytest.fixture(scope="session")
+def host(request):
+    lms_id = subprocess.check_output(
+        [
+            "docker",
+            "compose",
+            "-f",
+            "/etc/docker/compose/docker-compose.yaml",
+            "ps",
+            "-q",
+            "lms",
+        ]
+    ).strip()
+    yield testinfra.get_host(f"docker://{lms_id}")
+
+
+def test_lms_is_running(host):
+    assert host.file("/edx-platform/lms/static/images/logo.png").exists
