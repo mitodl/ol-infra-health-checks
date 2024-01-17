c02355fdc023ca7a7942aea2bf6733b0ea9cf85e
Chris Patti <feoh@feoh.org>
2024-01-17 17:02:50 -0500

    Fix: handle bytes -> str conversion and properly yield the fixture

--- tests/test_xpro_health_checks.py
+++ tests/test_xpro_health_checks.py
@@ -5,7 +5,7 @@ import testinfra
 
 @pytest.fixture(scope="session")
 def host(request):
-    lms_id = subprocess.check_output(
+    lms_id_bytes = subprocess.check_output(
         [
             "docker",
             "compose",
@@ -16,8 +16,9 @@ def host(request):
             "lms",
         ]
     ).strip()
+    lms_id = str(lms_id_bytes, encoding="utf-8")
     yield testinfra.get_host(f"docker://{lms_id}")
 
 
-def test_lms_is_running(host):
-    assert host.file("/edx-platform/lms/static/images/logo.png").exists
+def test_lms_has_static_assets(host):
+    assert host.file("/openedx/edx-platform/lms/static/images/logo.png").exists
