0ee2253bee805a92965310393d028940ae5596fe
Chris Patti <feoh@feoh.org>
2025-02-14 12:02:05 -0500

    Actually return an HTTP failure code unless the tests explicitly pass.

--- healthcheck_api/main.py
+++ healthcheck_api/main.py
@@ -1,11 +1,11 @@
-from fastapi import FastAPI  # type: ignore[import-not-found]
+from fastapi import FastAPI, Response, status# type: ignore[import-not-found]
 import subprocess
 
 healthcheck = FastAPI()
 
 
-@healthcheck.get("/healthcheck/{test_name}")
-async def root(test_name: str):
+@healthcheck.get("/healthcheck/{test_name}", status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
+async def root(test_name: str, response: Response):
     test_result = subprocess.run(
         [
             "/usr/local/bin/python3",
@@ -21,10 +21,13 @@ async def root(test_name: str):
     )
 
     test_returncode = test_result.returncode
-    test_status = not test_returncode
+    test_passed = not test_returncode
     test_output = test_result.stdout
 
+    if test_passed:
+        response.status_code = status.HTTP_200_OK
+
     return {
-        "test_status": test_status,
+        "test_status": test_passed,
         "test_output": test_output,
     }
